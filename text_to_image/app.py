29ec64665a290de9de4642b77af7577a53e1a969
Pawan Gawande <129038360+ppgawande12@users.noreply.github.com>
2023-11-06 16:05:04 +0530

    app.py

--- text_to_image/app.py
+++ text_to_image/app.py
@@ -9,21 +9,21 @@ from authtoken import auth_token
 
 app = Flask(__name__)
 
-# Load the stable diffusion model
+
 modelid = "CompVis/stable-diffusion-v1-4"
 device = "cuda"
 stable_diffusion_model = StableDiffusionPipeline.from_pretrained(
     modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
 stable_diffusion_model.to(device)
 
-# Define the route for the main page
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route to handle the image generation
+
 
 
 @app.route('/generate_image', methods=['POST'])
@@ -32,7 +32,7 @@ def generate_image():
     with autocast(device):
         image = stable_diffusion_model(text, guidance_scale=8.5)["sample"][0]
 
-    # Save the generated image to memory
+  
     img_io = io.BytesIO()
     image.save(img_io, format='PNG')
     img_io.seek(0)
